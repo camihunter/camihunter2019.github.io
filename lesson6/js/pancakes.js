8bbdcf7a63f62757a3b050cd261ec444dc5321c6
camihunter <hun18049@byui.edu>
2019-10-30 13:27:47 -0600

    v

--- lesson6/js/pancakes.js
+++ lesson6/js/pancakes.js
@@ -1,3 +1,3 @@
-const pancakes = new Date().getDay() === 1 ? "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." : none;
+const pancakes = new Date().getDay() === 6 ? "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." : none;
 
 document.getElementById('pancakebanner').innerhtml = pancakes;
