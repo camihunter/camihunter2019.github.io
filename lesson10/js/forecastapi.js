e9e91f8e6744db8e8d2c59a335de01df14726e03
camihunter <hun18049@byui.edu>
2019-11-27 00:57:24 -0700

    update

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -1,15 +1,16 @@
+//const weatherapiURL = "https://api.openweathermap.org/data/2.5/weather?" + id + "&units=imperial&APPID=80308be28fe0d83661b5b1c7d8efff8f";
 const weatherapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=80308be28fe0d83661b5b1c7d8efff8f";
 
 fetch(weatherapiURL)
     .then((response) => response.json())
 
     .then((jsObject) => {
-        console.log(jsObject);
+        //console.log(jsObject);
 
         document.getElementById('hightemp').textContent = jsObject.main.temp_max;
-        document.getElementById('temp').textContent = jsObject.main.temp;
+        document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
-        document.getElementById('windspeed').textContent = jsObject.wind.speed;
+        document.getElementById('windspeed').textContent = jsObject.wind.speed.toFixed(0);
         document.getElementById('description').textContent = jsObject.weather[0].main;
         let temp = document.getElementById('temp');
         let speed = document.getElementById('windspeed');
@@ -22,21 +23,38 @@ const forecastapiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604
 fetch(forecastapiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-            console.log(jsObject);
+        console.log(jsObject);
+
+
+        for (let i = 0; i < jsObject.list.length; i++) {
+            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+                let options = {
+                    weekday: 'short'
+                };
+                let weekday = document.createElement('th');
+                let rowdata = document.createElement('td');
+
+                weekday.textContent = new Date(jsObject.list[i].dt_txt).toLocaleDateString('en-US', options);
+                rowdata.textContent = jsObject.list[i].main.temp.toFixed(0);
+
+                //console.log(jsObject.list[i].main.temp.toFixed(0))
+                document.querySelector('.tablehead').appendChild(weekday);
+                document.querySelector('.tabledata').appendChild(rowdata);
+            }
+        }
     });
 
 
-            function windChill(temp, speed) {
-                var windchilltemp = 0;
-                if (temp <= 50 && speed > 3) {
-                    windchilltemp = 35.74 + (.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (.4275 * temp * Math.pow(speed, .16));
-                    windchilltemp = Math.round(windchilltemp);
-                    document.getElementById('windchill').innerHTML = windchilltemp + "&#176;F";;
-                } else {
-                    windchilltemp = "N/A";
-                    document.getElementById('windchill').textContent = windchilltemp;
-                }
-                //console.log("Current Temp = " + temp + ", Wind Speed = " + speed + ", Wind Chill = " + windchilltemp)
-                //console.log('windchilltemp');
-            };
-
+function windChill(temp, speed) {
+    var windchilltemp = 0;
+    if (temp <= 50 && speed > 3) {
+        windchilltemp = 35.74 + (.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (.4275 * temp * Math.pow(speed, .16));
+        windchilltemp = Math.round(windchilltemp);
+        document.getElementById('windchill').innerHTML = windchilltemp + "&#176;F";;
+    } else {
+        windchilltemp = "N/A";
+        document.getElementById('windchill').textContent = windchilltemp;
+    }
+    //console.log("Current Temp = " + temp + ", Wind Speed = " + speed + ", Wind Chill = " + windchilltemp)
+    //console.log('windchilltemp');
+};
--- lesson10/js/pancakes.js
+++ lesson10/js/pancakes.js
@@ -1,1 +1,14 @@
-new Date().getDay() === 6 ? document.getElementById('pancakebanner').innerhtml= 'Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.": "check";
+/*window.onload = function popup() {
+new Date().getDay() === 2 ? document.getElementById('pancakebanner').innerhtml= 'Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.': null;
+console.log(new Date().getDay());
+console.log('pancakebanner')}*/
+
+function popup() {
+    var banner;
+    if (new Date().getDay() == 2) {
+        banner = 'Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.';}
+        document.getElementById('pancakebanner').innerhtml= banner;
+         
+    console.log(new Date().getDay())
+    console.log(banner);
+    };
