00b3207d9882d398a426cecfd128da40cc1a2191
camihunter <hun18049@byui.edu>
2019-11-26 20:44:52 -0700

    v

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -18,25 +18,24 @@ fetch(weatherapiURL)
         windChill(temp.textContent, speed.textContent);
     });
 
-    const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&units=imperial&APPID=80308be28fe0d83661b5b1c7d8efff8f";
+/*const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&units=imperial&APPID=80308be28fe0d83661b5b1c7d8efff8f";
 fetch(forecastAPI)
     .then((response) => response.json())
     .then((jsObject) => {
-        //console.log(jsObject);
+            //console.log(jsObject);
+*/
 
 
-    
-function windChill(temp, speed) {
-    var windchilltemp = 0;
-    if (temp <= 50 && speed > 3) {
-        windchilltemp = 35.74 + (.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (.4275 * temp * Math.pow(speed, .16));
-        windchilltemp = Math.round(windchilltemp);
-        document.getElementById('windchill').innerHTML = windchilltemp + "&#176;F";
-        ;
-    } else {
-        windchilltemp = "N/A";
-        document.getElementById('windchill').textContent = windchilltemp;
-    }
-     console.log("Current Temp = " + temp + ", Wind Speed = " + speed + ", Wind Chill = " + windchilltemp)
-    console.log('windchilltemp');
-}
+            function windChill(temp, speed) {
+                var windchilltemp = 0;
+                if (temp <= 50 && speed > 3) {
+                    windchilltemp = 35.74 + (.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (.4275 * temp * Math.pow(speed, .16));
+                    windchilltemp = Math.round(windchilltemp);
+                    document.getElementById('windchill').innerHTML = windchilltemp + "&#176;F";;
+                } else {
+                    windchilltemp = "N/A";
+                    document.getElementById('windchill').textContent = windchilltemp;
+                }
+                console.log("Current Temp = " + temp + ", Wind Speed = " + speed + ", Wind Chill = " + windchilltemp)
+                console.log('windchilltemp');
+            }
